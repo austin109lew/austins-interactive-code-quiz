9e8b9abacf5a60e13d07c7cf4003dec55b97dbfc
Austin Lewandowski <austinlewandowski@Austins-MBP.lan>
2023-12-18 15:44:14 -0500

    Updated correct quiz answers

--- script.js
+++ script.js
@@ -2,65 +2,55 @@
 const questionText = document.getElementById('question-text');
 const answerOptions = document.getElementById('answer-options');
 const scoreDisplay = document.getElementById('score');
-const timerDisplay = document.getElementById('timer');
+const timerDisplay = document.getElementById('timer-display');
 const startButton = document.getElementById('start-button');
 const questionContainer = document.getElementById('question-container');
 const scoreContainer = document.getElementById('score-container');
-
-// Start Button
-startButton.addEventListener('click', startQuiz);
-
-function startQuiz() {
-  // Hide the start button
-  startButton.style.display = 'none';
-  
-  // Show the question container
-  questionContainer.style.display = 'block';
-
-  // Call the necessary functions to start the quiz
-  displayQuestion();
-  startTimer();
-}
-
-// Quiz Questions
-const quizQuestions = [
-  {
-  question: 'Commonly used data types DO NOT include:',               
-  options: ['strings', 'booleans', 'alerts', 'numbers'],
-  correctAnswer: 'strings' 
-},
-{ 
-  question: 'The condition in an if / else statement is enclosed within _____.',
-options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-correctAnswer: 'quotes' 
-},
-{
-   question: 'Arrays in JavaScript can be used to store ____.',
-options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-correctAnswer: 'numbers and strings' 
-},
-{ 
-  question: 'String values must be enclosed within ____ when being assigned to variables',
-options: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-correctAnswer: 'commas' 
-},
-{ 
-  question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-options: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-correctAnswer: 'JavaScript' 
-},
-];
 
 // Initialize variables
 let currentQuestionIndex = 0;
 let score = 0;
 let timer;
+let timerValue = 60;
+
+// Start Button
+startButton.addEventListener('click', startQuiz);
+
+// Quiz Questions
+const quizQuestions = [
+  {
+    question: 'Commonly used data types DO NOT include:',
+    options: ['strings', 'booleans', 'alerts', 'numbers'],
+    correctAnswer: 'alerts',
+  },
+  {
+    question: 'The condition in an if / else statement is enclosed within _____.',
+    options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    correctAnswer: 'parentheses',
+  },
+  {
+    question: 'Arrays in JavaScript can be used to store ____.',
+    options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    correctAnswer: 'all of the above',
+  },
+  {
+    question: 'String values must be enclosed within ____ when being assigned to variables',
+    options: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    correctAnswer: 'quotes',
+  },
+  {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    options: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    correctAnswer: 'console.log',
+  },
+];
+
 
 // Display current question
 function displayQuestion() {
   const currentQuestion = quizQuestions[currentQuestionIndex];
   questionText.textContent = `Question ${currentQuestionIndex + 1}: ${currentQuestion.question}`;
-  
+
   answerOptions.innerHTML = '';
   currentQuestion.options.forEach(option => {
     const li = document.createElement('li');
@@ -79,6 +69,7 @@ function handleAnswerClick(event) {
 
   if (selectedAnswer === currentQuestion.correctAnswer) {
     score++;
+    scoreDisplay.textContent = score; // Update the score display
   }
 
   currentQuestionIndex++;
@@ -97,13 +88,13 @@ function nextQuestion() {
 
 // Implement a timer
 function startTimer() {
-  let timeLeft = 60;
+  timerDisplay.textContent = timerValue;
 
   timer = setInterval(() => {
-    timeLeft--;
-    timerDisplay.textContent = timeLeft;
+    timerValue--;
+    timerDisplay.textContent = timerValue;
 
-    if (timeLeft === 0) {
+    if (timerValue === 0) {
       endQuiz();
     }
   }, 1000);
@@ -126,3 +117,15 @@ function endQuiz() {
 answerOptions.addEventListener('click', handleAnswerClick);
 
 // Start the quiz
+function startQuiz() {
+  // Hide the start button
+  startButton.style.display = 'none';
+
+  // Show the question container
+  questionContainer.style.display = 'block';
+
+  // Call the necessary functions to start the quiz
+  displayQuestion();
+  startTimer();
+}
+
